3bd0728dfe110602edf4c7c429baf6fbd0fad00f
chen_aliyun <1871471875@qq.com>
2024-10-15 17:00:32 +0800

    解决报错问题

--- train.py
+++ train.py
@@ -4,5 +4,6 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # for i in range(3):
     #     print(f"第{i+1}次训练")
-    model = YOLO('yolov8n.yaml').load('yolov8n.pt')
-    results = model.train(data="LROC.yaml",epochs=200,imgsz=608)
+    model = YOLO('yolo11n.yaml').load("yolo11n.pt")
+    results = model.train(data="LROC.yaml",epochs=200,imgsz=640)
+
